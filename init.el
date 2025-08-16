f85a042e9c90c8af5115370ff644926c5f1a0e92
Karan Swansi <kswansi2002@gmail.com>
2025-08-16 16:57:40 -0400

    added some formatting, added dirvish, removed menu-bar-mode, and tool-bar-mode

--- init.el
+++ init.el
@@ -3,7 +3,6 @@
 	     '("melpa" . "https://melpa.org/packages/") t)
 
 (package-initialize)
-
 ;; enables lsp-mode in all programming modes we can detec
 (use-package lsp-mode
   :init
@@ -14,12 +13,11 @@
          (lsp-mode . lsp-enable-which-key-integration))
   :commands lsp)
 
-
 (use-package ace-window
   :ensure t
   :bind (("M-o" . ace-window)))
 
-(use-package ivy
+(use-package ivy 
   :ensure t
   :config
   (ivy-mode 1))
@@ -27,28 +25,31 @@
 (use-package magit
   :ensure t)
 
+(use-package org
+  :ensure t)
 
-;; (setq magit-section-initial-visibility-alist
-;;      '((untracked . show)))
+(use-package org-roam
+  :ensure)
 
+(use-package dirvish
+  :ensure t)
 ;; shorter other window keybinding
 (global-set-key (kbd "M-o") 'other-window) 
-
-;; arrow key keybinding for window navigation
-(windmove-default-keybindings)
-
 ;; ace-window for tied to M-o
 (global-set-key (kbd "M-o") 'ace-window)
 
-;; right alt to regular alt (alt-gr?) for ergonomics
 (setq w32-recognize-altgr nil)
+(setq inhibit-startup-screen t)
+(setq display-line-numbers-type 'relative)
+(setq mouse-drag-and-drop-region t)
 
 ;; UI Improvements
-(column-number-mode)
 (global-display-line-numbers-mode)
+(column-number-mode)
 (tool-bar-mode -1)
+(menu-bar-mode -1)
 
-;; eisable lin numbers for some modes
+;; disable line numbers for some modes
 (dolist (mode '(term-mode-hook
 		eshell-mode-hook))
   (add-hook mode(lambda () (display-line-numbers-mode 0))))
@@ -61,9 +62,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(leuven-dark))
- '(global-tab-line-mode t)
  '(package-selected-packages
-   '(ace-window ivy lsp-mode magit magit-find-file vdiff-magit)))
+   '(ace-window dirvish ivy lsp-mode magit magit-find-file vdiff-magit)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
